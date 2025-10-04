9414737399397dc48fa25b9ac091f1b2bc27f2a6
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2025-10-04 18:12:10 +0200

    chore: fix various linting errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import hideoo from '@hideoo/eslint-config'
 
 export default hideoo([
   {
-    files: ['src/libs/theme.ts'],
+    files: ['src/libs/theme.ts', 'src/themes/components/ThemeImage.astro'],
     rules: {
       'unicorn/no-await-expression-member': 'off',
     },
