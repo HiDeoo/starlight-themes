59f58be987eb9a6ffeb4c060adad049f2568f9d6
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2026-06-10 12:31:06 +0200

    feat: add lucode theme (#14)

--- src/dev.ts
+++ src/dev.ts
@@ -6,7 +6,7 @@ import type { ThemeId } from './libs/theme'
 await run()
 
 async function run() {
-  await devStarlight('celestia')
+  await devStarlight()
 }
 
 async function devStarlight(id?: ThemeId) {
